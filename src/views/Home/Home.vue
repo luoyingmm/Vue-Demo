d9309c98100d97fe04019fff51a02b91ad03f3b8
luoyingmm <1502972236@qq.com>
2021-11-02 22:41:41 +0800

    Home Page

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -3,58 +3,85 @@
     <el-col :span="8" style="margin-top: 20px">
       <el-card shadow="hover">
         <div class="user">
-          <img :src="userImg"/>
+          <img :src="userImg" />
           <div class="userinfo">
             <p class="name">落樱</p>
             <p class="access">超级管理员</p>
           </div>
         </div>
         <div class="login-info">
-          <p>上次登录时间: <span>2021-11-1</span></p>
-          <p>上次登录地点: <span>江苏</span></p>
+          <p>上次登录时间：<span>2021-11-2</span></p>
+          <p>上次登录地点：<span>江苏</span></p>
         </div>
       </el-card>
-      <el-card style="height: 550px;margin-top: 20px">
+      <el-card style="margin-top: 20px; height: 460px">
         <el-table :data="tableData">
-          <el-table-column show-overflow-tooltip  v-for="(item,key) in tableLabel" :key="key" :prop="key" :label="item">
-
+          <el-table-column
+            show-overflow-tooltip
+            v-for="(val, key) in tableLabel"
+            :key="key"
+            :prop="key"
+            :label="val"
+          >
           </el-table-column>
         </el-table>
       </el-card>
     </el-col>
     <el-col :span="16" style="margin-top: 20px">
       <div class="num">
-        <el-card shadow="hover" v-for="item in countData" :key="item.name" :body-style="{display: 'flex',padding: 0}">
-          <i class="icon" :class="`el-icon-${item.icon}`" :style="{background: item.color}"></i>
+        <el-card
+          shadow="hover"
+          v-for="item in countData"
+          :key="item.name"
+          :body-style="{ display: 'flex', padding: 0 }"
+        >
+          <i
+            class="icon"
+            :class="`el-icon-${item.icon}`"
+            :style="{ background: item.color }"
+          ></i>
           <div class="detail">
-            <p class="num">￥{{item.value}}</p>
-            <p class="txt">￥{{item.name}}</p>
+            <p class="num">￥{{ item.value }}</p>
+            <p class="txt">￥{{ item.name }}</p>
           </div>
         </el-card>
       </div>
-      <el-card shadow="hover" style="height: 280px"></el-card>
+      <el-card shadow="hover" style="height: 280px">
+        <!-- <div style="height: 280px" ref="echart"></div> -->
+        <echart :chartData="echartData.order" style="height: 280px"></echart>
+      </el-card>
       <div class="graph">
-        <el-card shadow="hover" style="height: 280px"></el-card>
-        <el-card shadow="hover" style="height: 280px"></el-card>
+        <el-card shadow="hover" style="height: 260px">
+          <echart :chartData="echartData.user" style="height: 240px"></echart>
+        </el-card>
+        <el-card shadow="hover" style="height: 260px">
+          <echart
+            :chartData="echartData.video"
+            style="height: 240px"
+            :isAxisChart="false"
+          ></echart>
+        </el-card>
       </div>
     </el-col>
   </el-row>
 </template>
 
 <script>
-import {getHome} from '../../api/data'
-import {getMenu} from '../../api/data'
+import { getHome } from "../../api/data";
+import Echart from "@/components/ECharts.vue";
 export default {
-  name: 'Home',
-   data(){
-    return{
+  components: {
+    Echart,
+  },
+  data() {
+    return {
       userImg: require("../../assets/images/user.jpg"),
-      tableData:[],
-      tableLabel:{
-        name: '手机品牌',
-        totalBuy: '今日购买',
-        monthBuy: '本月购买',
-        totalBuy: '总购买'
+      tableData: [],
+      tableLabel: {
+        name: "课程",
+        totalBuy: "今日购买",
+        monthBuy: "本月购买",
+        totalBuy: "总购买",
       },
       countData: [
         {
@@ -94,21 +121,71 @@ export default {
           color: "#5ab1ef",
         },
       ],
-    }
-   },
-  mounted () {
-    this.getTableData()
+      echartData: {
+        order: {
+          xData: [],
+          series: [],
+        },
+        user: {
+          xData: [],
+          series: [],
+        },
+        video: {
+          series: [],
+        },
+      },
+    };
   },
-  methods:{
-    getTableData(){
-      getHome().then((res)=>{
-        this.tableData = res.data.tableData
-      })
-    }
-  }
-}
+  methods: {
+    getTableData() {
+      getHome().then((res) => {
+        // console.log(res);
+        this.tableData = res.data.tableData;
+
+        // 折线图的展示
+        const order = res.data.orderData;
+        let keyArray = Object.keys(order.data[0]);
+
+        // const myEchartsOrder = echarts.init(this.$refs.echart);
+        // myEchartsOrder.setOption(this.echartsData.order);
+
+        // 传给组件的值
+        this.echartData.order.xData = order.date;
+        keyArray.forEach((key) => {
+          this.echartData.order.series.push({
+            name: key,
+            data: order.data.map((item) => item[key]),
+            type: "line",
+          });
+        });
+
+        // 用户图
+
+        this.echartData.user.xData = res.data.userData.map((item) => item.date);
+        this.echartData.user.series.push({
+          name: "新增用户",
+          data: res.data.userData.map((item) => item.new),
+          type: "bar",
+        });
+        this.echartData.user.series.push({
+          name: "活跃用户",
+          data: res.data.userData.map((item) => item.active),
+          type: "bar",
+        });
+
+        this.echartData.video.series.push({
+          data: res.data.videoData,
+          type: "pie",
+        });
+      });
+    },
+  },
+  mounted() {
+    this.getTableData();
+  },
+};
 </script>
 
-<style lang="scss" scoped >
-@import "~@/assets/scss/home"
+<style lang="scss" scoped>
+@import "~@/assets/scss/home";
 </style>
--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
   lintOnSave: false,
   devServer: {
     //host: '0.0.0.0',
-    public : '192.168.200.193:8080' , //修改这里
+    public : '192.168.1.6:8080' , //修改这里
     https : false ,
     hotOnly : false ,
   }
